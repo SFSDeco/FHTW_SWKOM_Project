1f18ec8a786183826c48679e17ce606af794f355
SFSDeco <david.kim.ung@gmail.com>
2024-12-09 19:32:32 +0100

    added Info logging in DocumentProducer.javar and finalized elasticsearch integration

--- backend/rest/src/main/java/at/fhtw/rest/service/rabbitmq/DocumentProducer.java
+++ backend/rest/src/main/java/at/fhtw/rest/service/rabbitmq/DocumentProducer.java
@@ -1,12 +1,17 @@
 package at.fhtw.rest.service.rabbitmq;
 
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
 public class DocumentProducer {
 
+    private static final Logger log = LoggerFactory.getLogger(DocumentProducer.class);
     private final RabbitTemplate rabbitTemplate;
 
     @Autowired
@@ -15,6 +20,7 @@ public class DocumentProducer {
     }
 
     public void sendDocumentEvent(String documentMessage) {
+        log.info("Sent {} to RabbitMQ", documentMessage);
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY, documentMessage);
     }
 }
